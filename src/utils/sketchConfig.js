b0ba62594f666bd6abc6e1a5ac7dad1d00e3b324
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-10-22 17:10:34 +0200

    Abstract the Sketch API

---
+++ src/utils/sketchConfig.js
@@ -0,0 +1,13 @@
+import sketch from 'sketch';
+
+export const sketchAlert = (message) => sketch.UI.message(message);
+
+export default (context) => ({
+  get doc() {
+    return sketch.fromNative(context.document);
+  },
+
+  get primitivesPage() {
+    return this.doc.pages.filter(page => page.name === 'primitives')
+  }
+})
