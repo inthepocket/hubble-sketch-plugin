dadd24febd6593a3d8c3796e61fc2f46ff1e5769
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-10-24 11:18:41 +0200

    Add more sketch UI fns + document settings helpers

--- src/utils/sketchConfig.js
+++ src/utils/sketchConfig.js
@@ -1,17 +1,18 @@
 import sketch from 'sketch';
 
 export const sketchAlert = message => sketch.UI.message(message);
+export const sketchPrompt = (message, defaultValue) => sketch.UI.getStringFromUser(message, defaultValue);
+export const sketchInput = (message, options) => sketch.UI.getSelectionFromUser(message, options);
 
 export default context => ({
   get doc() {
     return sketch.fromNative(context.document);
   },
 
-  get primitivesPage() {
-    return this.doc.pages.filter(page => page.name === 'primitives').find(Boolean);
+  get hasDocumentSetting() {
+    const val = sketch.Settings.documentSettingForKey(this.doc, this.documentSettingsKey);
+    return !val || val === 'null' ? false : val;
   },
 
-  get assetOutPutDir() {
-    return '~/Desktop/sketchxport-output/svg';
-  },
+  documentSettingsKey: 'sketchxport-project-id',
 });
