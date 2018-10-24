8a9dce72a6df6310b05a70013bb9d71237726138
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-10-24 10:05:48 +0200

    do some thib bash magic

--- src/sketchxport.js
+++ src/sketchxport.js
@@ -1,9 +1,9 @@
 // documentation: https://developer.sketchapp.com/reference/api/
-import {startOfPlugin, endOfPlugin} from './utils/debug';
+import { startOfPlugin, endOfPlugin } from './utils/debug';
 import generateConfig from './services/generateConfig';
 import generateAssets from './services/generateAssets';
 import playSystemSound from './utils/playSystemSound';
-import sketchConfig, {sketchAlert} from './utils/sketchConfig';
+import sketchConfig, { sketchAlert } from './utils/sketchConfig';
 
 const debugConfig = {
   debugEnabled: true,
@@ -11,37 +11,33 @@ const debugConfig = {
   withFailureSound: true,
 };
 
-export default function (context) {
-  startOfPlugin (true);
+export default function(context) {
+  startOfPlugin(true);
 
-  const {doc, primitivesPage, assetOutPutDir} = sketchConfig (context);
+  const { doc, primitivesPage, assetOutPutDir } = sketchConfig(context);
 
   try {
-    sketchAlert ('Aaight, we catch your drift and start exporting. 🙌');
+    sketchAlert('Aaight, we catch your drift and start exporting. 🙌');
     if (!context) {
-      throw new Error ('Plugin should have context');
+      throw new Error('Plugin should have context');
     }
 
     if (!doc && !doc.pages) {
-      throw new Error ('The doc should have pages..');
+      throw new Error('The doc should have pages..');
     }
 
     // const sharedStyle = doc.getSharedTextStyles();
 
-    generateConfig (primitivesPage);
-    generateAssets (primitivesPage, assetOutPutDir);
+    generateConfig(primitivesPage);
+    generateAssets(primitivesPage, assetOutPutDir);
 
-    if (debugConfig.withSuccessSound) playSystemSound ('Glass');
+    if (debugConfig.withSuccessSound) playSystemSound('Glass');
 
-    endOfPlugin ();
+    endOfPlugin();
   } catch (e) {
-    console.error (
-      '😿 😿 😿 😿 😿 😿 😿 😿 😿\n',
-      e,
-      '\n😿 😿 😿 😿 😿 😿 😿 😿 😿'
-    );
+    console.error('😿 😿 😿 😿 😿 😿 😿 😿 😿\n', e, '\n😿 😿 😿 😿 😿 😿 😿 😿 😿');
 
-    if (debugConfig.withFailureSound) playSystemSound ('Basso');
+    if (debugConfig.withFailureSound) playSystemSound('Basso');
     throw e;
   }
 }
