9f766320ac8fdf21017b05818daf592c834e0af8
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-03-20 14:55:18 +0100

    Remove useless config

--- src/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Development and environmental config for the plugin.
- */
-export default {
-  debugEnabled: process.env.NODE_ENV !== 'production',
-  withSuccessSound: true,
-  withFailureSound: true,
-}
